a34bd1bb06b67408cea720bdde72f8a8c2db5e4e
20221011_SERVLET <leonas293@naver.com>
2023-04-20 04:23:34 +0000

    웹프로그래밍7주차 1

--- js/search.js
+++ js/search.js
@@ -1,13 +1,14 @@
-document.getElementById("form_main").addEventListener('submit', search_message);
+document.getElementById("search_btn").addEventListener('click', search_message);
 
-var search_array = []; // 빈 배열 – 전역 변수
+function search_message(){
 
-
-function search_message(event) {
-    event.preventDefault(); // Prevent form submission
-    alert("검색을 수행합니다!");
-       search_array.push(search_str.value); // 배열에 검색어 추가
-       let text = document.getElementById("search_message").innerHTML = search_array.toString(); // 값 변환
-        document.querySelector("#form_main").submit();
+let search_str = document.querySelector("#search_txt");
+    if(search_str.value.length === 0){ // 문자 길이, 엄격한 비교
+       alert("검색어가 비었습니다. 입력해주세요"); 
     }
-
+    else{
+       alert("검색을 수행합니다!");
+       let text = document.getElementById("search_message").innerHTML = search_str.value;
+       document.querySelector("#form_main").submit();
+    }
+}
