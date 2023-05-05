a47ac259b046a3cb932d831878acbde241310c70
20221011_SERVLET <leonas293@naver.com>
2023-05-05 14:40:13 +0000

    웹프로그래밍 9주차 2

--- js/login.js
+++ js/login.js
@@ -2,7 +2,7 @@ function login(){
     let form = document.querySelector("#form_main");
     let id = document.querySelector("#floatingInput");
     let password = document.querySelector("#floatingPassword");
-    
+ 
     form.action = "../index_login.html";
     form.method = "get"
     
@@ -10,5 +10,27 @@ function login(){
         alert("아이디와 비밀번호를 모두 입력해주세요.")
     }else{
         form.submit();
+        get_id();
     }
 }
+function logout(){
+    location.href='../index.html';
+}
+
+function get_id(){
+    var getParameters = function(paramName){
+    var returnValue;
+    var url = location.href;
+    var parameters = (url.slice(url.indexOf('?') + 1, url.length)).split('&');
+        for(var i = 0; i < parameters.length; i++) { 
+            var varName = parameters[i].split('=')[0];
+            
+            if (varName.toUpperCase() == paramName.toUpperCase()) {
+                returnValue = parameters[i].split('=')[1];
+                return decodeURIComponent(returnValue);
+            }
+        }
+};
+alert(getParameters('id') + '님 방갑습니다!');
+}
+
--- js/search.js
+++ js/search.js
@@ -1,18 +1,18 @@
-document.getElementById("search_btn").addEventListener('click', search_message);
-
 var search_array = []; // 빈 배열 – 전역 변수
+var slangs = ["idiot", "바보", "멍청이"]; // 욕설 리스트
 
-function search_message(){
-
-let search_str = document.querySelector("#search_txt");
-    if(search_str.value.length === 0){ // 문자 길이, 엄격한 비교
-       alert("검색어가 비었습니다. 입력해주세요"); 
-    }
-    else{
-       alert("검색을 수행합니다!");
-        search_array.push(search_str.value); // 배열에 검색어 추가
-        let text = document.getElementById("search_message").innerHTML = search_array.toString(); // 값 변환
-
-        document.querySelector("#form_main").submit();
-    }
+function search_message() { 
+  let search_str = document.querySelector("#search_txt");
+  if (search_str.value.length === 0) { // 문자 길이, 엄격한 비교
+    alert("검색어가 비었습니다. 입력해주세요"); 
+  } else if (slangs.includes(search_str.value.toLowerCase())) { // 욕설 체크
+    alert(search_str.value + "는 검색어로 적절하지 않습니다.");
+  } else {
+    alert("검색을 수행합니다!");
+    search_array.push(search_str.value); // 배열에 검색어 추가
+    let text = document.getElementById("search_message").innerHTML = search_array.toString(); // 값 변환
+    document.querySelector("#form_main").submit();
+  }
 }
+
+document.getElementById("search_btn").addEventListener('click', search_message);
